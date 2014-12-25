d11b945cdc05f4175cf6298b48fe267121a63a5f
Wyman Tan <wymanhuynh@gmail.com>
2014-12-25 09:23:11 -0800

    Define pixel size

--- sketchpad/script.js
+++ sketchpad/script.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
     var pixel_row = 16;
     var pixel_col = 16;
     var pixel = '<div class="pixel"></div>';
+    var pixel_size = $('#pixel_container').width() - 2; //-2 for border size
     var light_on = function() {
         $(this).addClass('highlight');
     };
+
+    //define pixel size
+    $('.pixel').css('width', pixel_size);
+    $('.pixel').css('height', pixel_size);
 
     //populate pixel_container
     for(var i = 0; i < pixel_row; i++) {

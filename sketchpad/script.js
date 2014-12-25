384a198bedc13e22b869ce7540568cbaa9774886
Wyman Tan <wymanhuynh@gmail.com>
2014-12-25 09:58:57 -0800

    Fix pixel size

--- sketchpad/script.js
+++ sketchpad/script.js
@@ -2,23 +2,25 @@ $(document).ready(function() {
     var pixel_row = 16;
     var pixel_col = 16;
     var pixel = '<div class="pixel"></div>';
-    var pixel_size = $('#pixel_container').width() - 2; //-2 for border size
+    var new_row = '<div class="new_row"></div>';
+    var pixel_size = +$('#pixel_container').width() / pixel_row;
     var light_on = function() {
         $(this).addClass('highlight');
     };
-
-    //define pixel size
-    $('.pixel').css('width', pixel_size);
-    $('.pixel').css('height', pixel_size);
 
     //populate pixel_container
     for(var i = 0; i < pixel_row; i++) {
         for(var j = 0; j < pixel_col; j++) {
             $('#pixel_container').append(pixel);
         }
-        $('.pixel').last().addClass('.new_row');
+            $('#pixel_container').append(new_row);
     }
-
+    
+    //define pixel size
+    $('.pixel').css('width', pixel_size);
+    $('.pixel').css('height', pixel_size);
+    
+    //highlight pixels on mouse enter
     $('#pixel_container').on('mouseenter', '.pixel', light_on);
 
 });

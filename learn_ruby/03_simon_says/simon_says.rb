db072b1bf62de4c339dff97d6c15d2a2b3a27c7e
Wyman Tan <wymanhuynh@gmail.com>
2015-01-03 14:46:52 -0800

    Include title prepositions

--- learn_ruby/03_simon_says/simon_says.rb
+++ learn_ruby/03_simon_says/simon_says.rb
@@ -20,7 +20,7 @@ def first_word(string)
 end
 
 def titleize(string)
-    lowercase_word = %w{a an the and but or for nor of over}
+    lowercase_word = %w{a an the and or nor of over aboard about above across after against along amid among anti around as at before behind below beneath beside besides between beyond but by concerning considering despite down during except excepting excluding following for from in inside into like minus near of off on onto opposite outside over past per plus regarding round save since than through to toward towards under underneath unlike until up upon versus via with within without}
     title = string.split.map do |word|
         if lowercase_word.include?(word)
             word

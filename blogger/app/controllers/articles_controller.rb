653d586e12f4c9b99303e7345637f493288950d5
Wyman Tan <wymanhuynh@gmail.com>
2015-01-10 14:19:46 -0800

    Articles update and create/update form partial

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -13,9 +13,19 @@ class ArticlesController < ApplicationController
         @article = Article.new 
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(article_params)
         @article.save
+        redirect_to article_path(@article)
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
         redirect_to article_path(@article)
     end
 
@@ -24,4 +34,6 @@ class ArticlesController < ApplicationController
         @article.destroy
         redirect_to articles_path
     end
+
+
 end

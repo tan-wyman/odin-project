4bea90eb24e64371ca801d9bc806a3c3d266764b
Wyman Tan <wymanhuynh@gmail.com>
2015-02-07 22:16:12 -0800

    Article models, views, and forms

---
+++ blog/app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@aarticle)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destory
+
+    redirect_to articles_path
+  end
+
+
+
+end
+
+
